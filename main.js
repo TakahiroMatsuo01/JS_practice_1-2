afeb19cb17ea2af6e6567b2b09555ae2a876d218
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-01-06 05:33:11 +0900

    課題作成後

---
+++ main.js
@@ -0,0 +1,10 @@
+const getProperty = function () {
+    let input = prompt("日本の首都は？");
+     if( input === "東京" ) {
+         confirm(　"正解です！"　);
+     }else if( input !== "東京" ) {
+         confirm( "不正解です！" );
+     }
+ }
+ 
+ getProperty();
