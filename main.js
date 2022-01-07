3e4616a60386bbf42f1d64f8a71f9996c8010fa5
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-01-07 19:58:35 +0900

    課題修正３

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const getProperty = function () {
-    let input = prompt("日本の首都は？");
-     if( input === "東京" ) {
-         confirm(　"正解です！"　);
-     } else if( input !== "東京" ) {
-         alert( "不正解です！" );
-     }
- }
- 
- getProperty();
+const getProperty = () => {
+   let input = prompt("日本の首都は？");
+    if( input === "東京" ) {
+        confirm(　"正解です！"　);
+    }else if( input !== "東京" ) {
+        alert( "不正解です！" );
+  }
+}
+while(getProperty.input!=="東京"){
+     getProperty();
+    break;
+}
