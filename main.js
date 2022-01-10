6095ea93930c812ea61d752046312c5cd949f2c8
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-01-10 16:45:37 +0900

    最終修正

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const getProperty = () => {
-   let input = prompt('日本の首都は？');
-   while(true) {
-        if(input === '東京') {
-        alert('正解です！');
-        break;
-    } else {
-        alert('不正解です！');
-        return getProperty();
-  　}
-　}
+  let input = prompt('日本の首都は？');
+    while(true) {
+     if(input === '東京') {
+      alert('正解です！');
+       break;
+      } else {
+      alert('不正解です！');
+      return getProperty();
+   　}
+　 }
 }
 getProperty();
